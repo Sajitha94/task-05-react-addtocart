437d670d5d64e4f5cf92385ff0aef4126750238f
Sajitha94 <sajithasubramani94@gmail.com>
2025-08-08 11:20:37 +0530

    bug fix

--- src/card.jsx
+++ src/card.jsx
@@ -12,19 +12,19 @@ export default function Card() {
   }, []);
 
   return (
-    <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-8 mx-10">
+    <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 md:gap-8 gap-3 md:mx-10 px-2">
       {products.map((products,index) => (
-        <div className="flex flex-col gap-5 border border-1 rounded-xl  p-3 justify-center  " key={index}>
-          <div className="w-62">
-            <img src={products.image} alt="card-img" className="rounded-xl w-full h-48 object-contain" />
+        <div className="flex flex-col md:gap-5 gap-2 border border-1 rounded-xl  md:p-3 justify-center p-2 " key={index}>
+          <div className="w-38 ">
+            <img src={products.image} alt="card-img" className="rounded-xl w-full md:h-48 h-24 object-contain" />
           </div>
-          <div className="flex justify-between">
-            <div className="flex fle-col text-sm line-clamp-2">
+          
+            <div className="flex fle-col text-sm mx-3">
               <h6 className="font-semibold truncate">{products.title}</h6>
             </div>
-            </div>
-            <div className="flex flex-col gap-3 justify-between">
-              <small className="text-gray-600 text-[16px] ">
+           
+            <div className="flex flex-col md:gap-3 gap-1 justify-between mx-2">
+              <small className="text-gray-600 text-[16px] text-center ">
                 Price: <span>₹ {products.price}</span>
               </small>
               <button className="border border-2 px-3 py-1 rounded-md hover:bg-gray-100">
