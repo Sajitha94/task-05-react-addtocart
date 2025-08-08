27928660f992909bfa3f3d288eed9fe6cf1cad4a
Sajitha94 <sajithasubramani94@gmail.com>
2025-08-08 11:22:07 +0530

    bug

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ import { ShoppingCart } from 'lucide-react';
 export default function Header(){
     return(
         <div className="border-b-2 border-gray-100 ">
-            <div className='mx-10 flex justify-between items-center py-2'>
+            <div className='md:mx-10 mx-2 flex justify-between items-center py-2'>
             <div className='flex items-center justify-center gap-2 '>
                 <img src={logo} alt="logo"  className="w-16 h-16 object-contain rounded-lg"/>
                 <h6 className="text-[#D4AF37] text-lg font-semibold">Niha Luxe</h6>
